db13becfb7e3658e6303020a7c957485998969ca
AlaaSaqa <92124254+AlaaSaqa@users.noreply.github.com>
2022-09-22 11:26:23 +0300

    Time: 677 ms (18.42%), Space: 47.7 MB (44.07%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cs
+++ 217-contains-duplicate/217-contains-duplicate.cs
@@ -1,11 +1,11 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
         List<int> arr = new List<int>();
-        foreach(var num in nums){
-           if (arr.Contains(num))
+        for(var i = 0 ; i< nums.Length ; i++){
+           if (arr.Contains(nums[i]))
                 return true;
             else 
-                arr.Add(num);
+                arr.Add(nums[i]);
        }
         return false;
     }
